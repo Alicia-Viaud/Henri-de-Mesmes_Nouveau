778890979b1519da6e0549232bd8d527065b4b6c
Alicia-Viaud <alicia.viaud@umontreal.ca>
2025-10-06 10:39:27 -0400

    Mise à jour

---
+++ variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# Fichier de variables (ignoré par Git)
+.
+# Chemin absolu vers l'exécutable de Saxon, à renseigner
+# ex. /Applications/Saxon/saxon.jar
+SAXON_PATH="/Applications/SaxonHE12-4J/saxon-he-12.4.jar"
+# Fichier .xsl, à renseigner
+XSL_PATH="custom.xsl"
+
